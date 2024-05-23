7b2aab5e74005d3597c97747476155ed7c06da46
JamilRez53 <89921897+JamilRez53@users.noreply.github.com>
2024-05-23 11:07:31 +0600

    Add changes to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div><p style={{color:"blue"}}>Home</p></div>
+    <div><p style={{color:"blue"}}>Home</p>
+    <p style={{color:"blue"}}>Content</p>
+    </div>
   )
 }
 
